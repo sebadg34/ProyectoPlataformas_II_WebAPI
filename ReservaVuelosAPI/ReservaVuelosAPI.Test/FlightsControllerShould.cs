f9b86c07b382bb78f56988e2f06158751099a41a
JuanBarnet <73753432+JuanBarnett@users.noreply.github.com>
2021-07-04 21:52:43 -0400

    feat(testBranch): creacion de test unitarios

---
+++ ReservaVuelosAPI/ReservaVuelosAPI.Test/FlightsControllerShould.cs
@@ -0,0 +1,181 @@
+﻿using Autofac.Extras.Moq;
+using ReservaVuelosAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Autofac;
+using Moq;
+using ReservaVuelosAPI.Controllers;
+using System.Web.Http;
+using System.Data.Entity.Infrastructure;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using System.Data.Entity.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace ReservaVuelosAPI.ReservaVuelosAPI.Test
+{
+    public class FlightsControllerShould
+    {
+        /// <summary>
+        /// Metodo para probar si el metodo GetFlight de FlightsController entrega corretamente a todos los vuelos
+        /// </summary>
+        [Fact]
+        public void GetFlightShould()
+        {
+            // Arrange
+            DBEntities db = new DBEntities();
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            _db.Setup(x => x.Flight).Returns(db.Flight);
+
+            FlightsController controller = new FlightsController();
+            controller.FlightsControllerTest(_db.Object, 0);
+
+            // Act
+            IQueryable<Flight> expected = db.Flight;
+            IQueryable<Flight> actual = controller.GetFlight();
+
+            // Assert
+            Assert.True(actual != null);
+            Assert.Equal(expected.Count(), actual.Count());
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo GetFlight dado un ID de FlightsController entrega correctamente el vuelo solicitado
+        /// </summary>
+        [Fact]
+        public void GetFlightIdShould()
+        {
+            // Arrange
+            int id = 1;
+            DBEntities db = new DBEntities();
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            _db.Setup(x => x.Flight.Find(id)).Returns(db.Flight.Find(id));
+
+            FlightsController controller = new FlightsController();
+            controller.FlightsControllerTest(_db.Object, 0);
+
+            // Act
+            IHttpActionResult actionResult = controller.GetFlight(id);
+            var contentResult = actionResult as OkNegotiatedContentResult<Flight>;
+
+            // Assert
+            Assert.NotNull(contentResult);
+            Assert.NotNull(contentResult.Content);
+            Assert.Equal(id, contentResult.Content.ID);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo GetFlight dado un ID de FlightsController es resistente a IDs inexitentes
+        /// </summary>
+        [Fact]
+        public void GetFlightIdShouldNot()
+        {
+            // Arrange
+            int id = 1000;
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            FlightsController controller = new FlightsController();
+            controller.FlightsControllerTest(_db.Object, 0);
+
+            // Act
+            IHttpActionResult actionResult = controller.GetFlight(id);
+            var contentResult = actionResult as NotFoundResult;
+
+            // Assert
+            Assert.IsType<NotFoundResult>(actionResult);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo GetLastFlight de FlightsController entrega el ultimo vuelo ingresado a la base de datos
+        /// </summary>
+        [Fact]
+        public void GetLastFlightShould()
+        {
+            // Arrange
+            DBEntities db = new DBEntities();
+
+            FlightsController controller = new FlightsController();
+
+            // Act
+            IHttpActionResult actionResult = controller.GetLastFlight();
+            var contentResult = actionResult as OkNegotiatedContentResult<Flight>;
+            int expected = db.Flight.Max(Flight => Flight.ID);
+
+            // Assert
+            Assert.NotNull(contentResult);
+            Assert.NotNull(contentResult.Content);
+            Assert.Equal(expected, contentResult.Content.ID);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo PostFlight de FlightsController ingresa correctamente un vuelo a la base de datos
+        /// </summary>
+        [Fact]
+        public void PostFlightShould()
+        {
+            // Arrange
+            Flight flight = new Flight("Premium", "Santiago", "Antofagasta", 40, 100, "101", new DateTime(2021, 07, 20, 20, 00, 00), new DateTime(2021, 07, 20, 23, 00, 00));
+            DBEntities db = new DBEntities();
+
+            FlightsController controller = new FlightsController();
+
+            // Act
+            IHttpActionResult actionResult = controller.PostFlight(flight);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Flight>;
+
+            try
+            {
+                if (createdResult.RouteName == "DefaultApi")
+                {
+                    db.Entry(flight).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // Assert
+            Assert.NotNull(createdResult);
+            Assert.Equal("DefaultApi", createdResult.RouteName);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo PostFlight de FlightsController es resistente a datos invalidos
+        /// </summary>
+        [Fact]
+        public void PostFlightShouldNotValidate()
+        {
+            // Arrange
+            Flight flight = new Flight(null, "Santiago", "Antofagasta", 40, 100, "101", new DateTime(2021, 07, 20, 20, 00, 00), new DateTime(2021, 07, 20, 23, 00, 00));
+            DBEntities db = new DBEntities();
+
+            FlightsController controller = new FlightsController();
+
+            // Act
+            IHttpActionResult actionResult = controller.PostFlight(flight);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Flight>;
+
+            try
+            {
+                if (createdResult.RouteName == "DefaultApi")
+                {
+                    db.Entry(flight).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // Assert
+            Assert.IsType<BadRequestResult>(actionResult);
+        }
+    }
+}
---
+++ ReservaVuelosAPI/ReservaVuelosAPI.Test/ManagersControllerShould.cs
@@ -0,0 +1,192 @@
+﻿using Autofac.Extras.Moq;
+using ReservaVuelosAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Autofac;
+using Moq;
+using ReservaVuelosAPI.Controllers;
+using System.Web.Http;
+using System.Data.Entity.Infrastructure;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using System.Data.Entity.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace ReservaVuelosAPI.ReservaVuelosAPI.Test
+{
+    public class ManagersControllerShould
+    {
+        /// <summary>
+        /// Metodo para probar si el metodo GetManager de ManagersController entrega corretamente a todos los administradores
+        /// </summary>
+        [Fact]
+        public void GetManagerShould()
+        {
+            // Arrange
+            DBEntities db = new DBEntities();
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            _db.Setup(x => x.Manager).Returns(db.Manager);
+
+            ManagersController controller = new ManagersController();
+            controller.ManagersControllerTest(_db.Object, 0);
+
+            // Act
+            IQueryable<Manager> expected = db.Manager;
+            IQueryable<Manager> actual = controller.GetManager();
+
+            // Assert
+            Assert.True(actual != null);
+            Assert.Equal(expected.Count(), actual.Count());
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo GetManager dado un ID de ManagersController entrega correctamente el administrador solicitado
+        /// </summary>
+        [Fact]
+        public void GetManagerIdShould()
+        {
+            // Arrange
+            int id = 3;
+            DBEntities db = new DBEntities();
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            _db.Setup(x => x.Manager.Find(id)).Returns(db.Manager.Find(id));
+
+            ManagersController controller = new ManagersController();
+            controller.ManagersControllerTest(_db.Object, 0);
+
+            // Act
+            IHttpActionResult actionResult = controller.GetManager(id);
+            var contentResult = actionResult as OkNegotiatedContentResult<Manager>;
+
+            // Assert
+            Assert.NotNull(contentResult);
+            Assert.NotNull(contentResult.Content);
+            Assert.Equal(id, contentResult.Content.ID);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo GetManager dado un ID de ManagersController es resistente a IDs inexitentes
+        /// </summary>
+        [Fact]
+        public void GetManagerIdShouldNot()
+        {
+            // Arrange
+            int id = 1000;
+            Mock<DBEntities> _db = new Mock<DBEntities>();
+            ManagersController controller = new ManagersController();
+            controller.ManagersControllerTest(_db.Object, 0);
+
+            // Act
+            IHttpActionResult actionResult = controller.GetManager(id);
+            var contentResult = actionResult as NotFoundResult;
+
+            // Assert
+            Assert.IsType<NotFoundResult>(actionResult);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo PostManager de ManagersController ingresa correctamente un administrador a la base de datos
+        /// </summary>
+        [Fact]
+        public void PostManagerShould()
+        {
+            // Arrange
+            Manager manager = new Manager("raul", "Castillo", "11286395-0", 100);
+            DBEntities db = new DBEntities();
+
+            ManagersController controller = new ManagersController();
+
+            // Act
+            IHttpActionResult actionResult = controller.PostManager(manager);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Manager>;
+
+            try
+            {
+                if (createdResult.RouteName == "DefaultApi")
+                {
+                    db.Entry(manager).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // Assert
+            Assert.NotNull(createdResult);
+            Assert.Equal("DefaultApi", createdResult.RouteName);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo PostManager de ManagersController es resistente a datos invalidos
+        /// </summary>
+        [Fact]
+        public void PostManagerShouldNotValidate()
+        {
+            // Arrange
+            Manager manager = new Manager("raul", "Castillo", "1128g395-0", 100);
+            DBEntities db = new DBEntities();
+
+            ManagersController controller = new ManagersController();
+
+            // Act
+            IHttpActionResult actionResult = controller.PostManager(manager);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Manager>;
+
+            try
+            {
+                if (createdResult.RouteName == "DefaultApi")
+                {
+                    db.Entry(manager).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // Assert
+            Assert.IsType<BadRequestResult>(actionResult);
+        }
+
+        /// <summary>
+        /// Metodo para probar si el metodo PostManager de ManagersController es resistente a datos unicos duplicadas
+        /// </summary>
+        [Fact]
+        public void PostManagerShouldNotDuplicate()
+        {
+            // Arrange
+            Manager manager = new Manager("raul", "Castillo", "11286395-0", 3);
+            DBEntities db = new DBEntities();
+
+            ManagersController controller = new ManagersController();
+
+            // Act
+            IHttpActionResult actionResult = controller.PostManager(manager);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Manager>;
+
+            try
+            {
+                if (createdResult.RouteName == "DefaultApi")
+                {
+                    db.Entry(manager).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // Assert
+            Assert.IsType<ConflictResult>(actionResult);
+        }
+    }
+}
